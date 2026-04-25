1565dac8016bf0a3b39c0b367202dc36af18e9ba
Muhammet Bağ <muhammetbag7@gmail.com>
2026-04-25 14:00:20 +0300

    feat: projenin tam paketi yüklendi

---
+++ src/html/utils/api-config.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = "http://testmb.runasp.net/api";
+
+export const apiUrl = (path) => {
+  if (!path) return API_BASE_URL
+  const normalizedPath = path.startsWith("/") ? path : `/${path}`
+  return `${API_BASE_URL}${normalizedPath}`
+}
